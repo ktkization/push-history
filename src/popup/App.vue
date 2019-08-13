be17a98825d0d88c26ed87b6d2f997125a997fc3
Kenneth Thuku <thuku.ken@live.com>
2019-08-13 19:06:08 +0300

    Add base layout to App component

--- src/popup/App.vue
+++ src/popup/App.vue
@@ -1,15 +1,27 @@
 <template>
   <div>
+    <nav class="nav">
+      <div class="nav-center bg-primary">
+        <a class="brand text-white">Push History</a>
+      </div>
+    </nav>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
 export default {
-  data () {
-    return {}
+  data() {
+    return {};
   }
-}
+};
 </script>
+
+<style>
+body {
+  min-width: 350px;
+  min-height: 450px;
+}
+</style>
 
 
--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App'
 
+import 'chota'
+
 import router from './router'
 
 
