c1a280aaead51c2c5be33a858f9adceaba023abf
Kenneth Thuku <thuku.ken@live.com>
2019-08-13 19:21:50 +0300

    Add Sign in button with handler

--- src/popup/router/pages/SignIn.vue
+++ src/popup/router/pages/SignIn.vue
@@ -1,15 +1,24 @@
 <template>
-  
+  <div>
+    <button class="button primary" id="signin" @click="signIn">Sign in with Pushbullet</button>
+  </div>
 </template>
 
 <script>
 export default {
-  data () {
-    return {}
+  data() {
+    return {};
+  },
+  methods: {
+    signIn() {
+      
+    }
   }
-}
+};
 </script>
 
 <style scoped>
-
+#signin {
+  margin: 20px 20px;
+}
 </style>
